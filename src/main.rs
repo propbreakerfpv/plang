b7e7ef19a132a785cd2a07837e2c9a053f764dfb
jonas breen <jonasebreen@gmail.com>
2024-03-18 22:12:25 -0400

    move test code to speret file

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,7 @@ mod parser;
 mod ast;
 
 fn main() {
-   let input = "fn println(msg: String) {}
-        fn display_value(value: i32) {
-            if value > 0 {
-                println(\"negitive\");
-            } else if value < 0 {
-                println(\"negitive\");
-            } else {
-                println(\"zero\");
-            }
-        }
-        fn main(arg: String, another: String) {
-        println(\"hello world\");
-        display_value(35);
-}";
-    // let input = fs::read_to_string("./test.pl").unwrap();
+    let input = fs::read_to_string("./test.plang").unwrap();
     println!("{}", input);
     let tokens = lexer::lex(input).into_iter().filter(|t| !t.is_whitespace()).collect();
     println!("{:?}", tokens);
